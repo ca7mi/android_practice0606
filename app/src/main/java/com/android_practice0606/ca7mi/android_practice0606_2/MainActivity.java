4bb37ad645cf9ee1d48cc2fed414d0fdc1850e90
Kanami Sugiyama <kanami.sugiyama@jp.ricoh.com>
2018-06-06 18:59:28 +0900

    動いた

---
+++ app/src/main/java/com/android_practice0606/ca7mi/android_practice0606_2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android_practice0606.ca7mi.android_practice0606_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
