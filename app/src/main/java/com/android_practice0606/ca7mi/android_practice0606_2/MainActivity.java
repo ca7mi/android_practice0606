aa638190bc67424c1740a4c54bdd625ca6954e0f
Kanami Sugiyama <kanami.sugiyama@jp.ricoh.com>
2018-06-07 13:38:17 +0900

    Twitterボタン作成

--- app/src/main/java/com/android_practice0606/ca7mi/android_practice0606_2/MainActivity.java
+++ app/src/main/java/com/android_practice0606/ca7mi/android_practice0606_2/MainActivity.java
@@ -24,10 +24,10 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.image_pocchama);
 
-        final Button button1 = findViewById(R.id.button_push);
+        final Button button1 = findViewById(R.id.button_select);
         button1.setOnClickListener(buttonClick);
 
-        final Button button2 = findViewById(R.id.button_tap);
+        final Button button2 = findViewById(R.id.button_twitter);
         button2.setOnClickListener(buttonClick);
     };
 
@@ -35,25 +35,37 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             switch (view.getId()) {
-                case R.id.button_push:
+                case R.id.button_select:
                     Log.d("debug","button_push, Perform action on click");
-
-
-
+                    /*
                     Intent intentGallery;
                     intentGallery = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                     intentGallery.addCategory(Intent.CATEGORY_OPENABLE);
                     intentGallery.setType("image/jpeg");
 
-                    startActivityForResult(intentGallery, REQUEST_CHOOSER);
+                    startActivityForResult(intentGallery, REQUEST_CHOOSER); */
+                    selectPicture();
                     break;
 
-                case R.id.button_tap:
+                case R.id.button_twitter:
                     Log.d("debug","button_tap, Perform action on click");
+                    postingCommentOnTwitter();
+                    break;
             }
         }
     };
 
+    // ギャラリーを開く
+    private void selectPicture(){
+        Intent intentGallery;
+        intentGallery = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intentGallery.addCategory(Intent.CATEGORY_OPENABLE);
+        intentGallery.setType("image/jpeg");
+
+        startActivityForResult(intentGallery, REQUEST_CHOOSER);
+    };
+
+    // ギャラリーから写真を選んでimageView_pocchamaに表示
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -64,24 +76,24 @@ public class MainActivity extends AppCompatActivity {
                 // キャンセル時
                 return ;
             }
-
             Uri resultUri = (data != null ? data.getData() : m_uri);
-
             if(resultUri == null) {
                 // 取得失敗
                 return;
             }
-
-           /* // ギャラリーへスキャンを促す
-            MediaScannerConnection.scanFile(
-                    this,
-                    new String[]{resultUri.getPath()},
-                    new String[]{"image/jpeg"},
-                    null
-            );*/
-
             // 画像を設定
             imageView.setImageURI(resultUri);
         }
     }
+
+    // Step1 コメントをTwitterで投稿
+    private void postingCommentOnTwitter(){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String message= Uri.encode("ポッチャマインテント ＃コメント");
+        intent.setData(Uri.parse("twitter://post?message=" + message));
+        startActivity(intent);
+    };
+
+    // Step2 画像を投稿
+
 }
