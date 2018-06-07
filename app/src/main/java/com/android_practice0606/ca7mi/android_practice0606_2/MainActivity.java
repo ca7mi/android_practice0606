277cf10cd2e9122e09cd1ff059accc6fb097f983
Kanami Sugiyama <kanami.sugiyama@jp.ricoh.com>
2018-06-07 16:42:31 +0900

    画像選んでないときにトーストメッセージが出せたら・・・

--- app/src/main/java/com/android_practice0606/ca7mi/android_practice0606_2/MainActivity.java
+++ app/src/main/java/com/android_practice0606/ca7mi/android_practice0606_2/MainActivity.java
@@ -6,16 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.net.Uri;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.widget.ImageView;
 import android.support.v4.app.ShareCompat;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class MainActivity extends AppCompatActivity {
 
     private Uri m_uri;
-    Uri resultUri = null;
+    private Uri resultUri = null;
     private static final int REQUEST_CHOOSER = 1000;
     private ImageView imageView = null;
 
@@ -38,19 +37,12 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             switch (view.getId()) {
                 case R.id.button_select:
-                    Log.d("debug","button_push, Perform action on click");
-                    /*
-                    Intent intentGallery;
-                    intentGallery = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-                    intentGallery.addCategory(Intent.CATEGORY_OPENABLE);
-                    intentGallery.setType("image/jpeg");
-
-                    startActivityForResult(intentGallery, REQUEST_CHOOSER); */
+                    Log.d("debug","button_select, Perform action on click");
                     selectPicture();
                     break;
 
                 case R.id.button_twitter:
-                    Log.d("debug","button_tap, Perform action on click");
+                    Log.d("debug","button_twitter, Perform action on click");
                     //postingCommentOnTwitter();
                     postingImageOnTwitter();
                     break;
@@ -105,16 +97,29 @@ public class MainActivity extends AppCompatActivity {
 
         ShareCompat.IntentBuilder builder = ShareCompat.IntentBuilder.from(this);
 
-// データをセットする
+        // データをセットする
         builder.setChooserTitle("Choose App");
         builder.setText(message);
         if(imagePath!=null){
             builder.setType("image/png");
             builder.addStream(resultUri);
+            // アプリ選択画面を起動
+            builder.startChooser();
         }else{
-            builder.setType("text/plain");
+            nonImege();
         }
-// アプリ選択画面を起動
-        builder.startChooser();
+    };
+
+    private void nonImege(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("写真を選んでから投稿しましょう！")
+                .setTitle("Information")
+                .setIcon(R.drawable.pocchama)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+// ボタンをクリックしたときの動作
+                    }
+                });
+        builder.show();
     };
 }
